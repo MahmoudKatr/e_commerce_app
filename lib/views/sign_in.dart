5301958fcf1e593d7ffb4a9561adce1078f02b75
mahmoudhossam <mahmoud.modo45@gmail.com>
2025-02-08 00:07:37 +0200

    Refactor SignIn view to include a Form widget and add formKey for validation✅

--- lib/views/sign_in.dart
+++ lib/views/sign_in.dart
@@ -18,6 +18,7 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -26,105 +27,110 @@ class _SignInState extends State<SignIn> {
       body: Padding(
         padding: const EdgeInsets.only(top: 120, left: 20, right: 20),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.network(
-                    'https://trangotech.com/blog/wp-content/uploads/2024/05/ecommerce-app-features.jpg'),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              const Text(
-                "Let's Get Started",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff272f54),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                      'https://trangotech.com/blog/wp-content/uploads/2024/05/ecommerce-app-features.jpg'),
                 ),
-              ),
-              const Text(
-                'Create an new account',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff9f9da8),
+                const SizedBox(
+                  height: 12,
                 ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              CustomTextFormField(
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Please enter your email address.';
-                  }
-                  if (!value.contains('@') || !value.contains('.')) {
-                    return 'Please enter a valid email address.';
-                  }
-                  return null;
-                },
-                controller: emailController,
-                hintText: 'Your Email',
-                prefix: Icons.email,
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              CustomTextFormField(
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Please enter a password.';
-                  }
-                  return null;
-                },
-                controller: passwordController,
-                hintText: 'Password',
-                prefix: Icons.lock,
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              CustomButton(
-                text: 'Sign In',
-                onPressed: () {},
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              const CustomDivider(titleDivider: 'OR'),
-              const SizedBox(height: 16),
-              SocialLoginButton(
-                icon: FontAwesomeIcons.google,
-                text: "Login with Google",
-                onPressed: () {},
-              ),
-              const SizedBox(height: 12),
-              SocialLoginButton(
-                icon: FontAwesomeIcons.facebook,
-                text: "Login with Facebook",
-                onPressed: () {},
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              CustomTextButton(
-                text: 'Forgot Password',
-                onPressed: () {},
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              CustomRow(
-                text: "Don't have an account?",
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const SignUp()));
-                },
-                textButton: 'Register',
-              ),
-            ],
+                const Text(
+                  "Let's Get Started",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff272f54),
+                  ),
+                ),
+                const Text(
+                  'Create an new account',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff9f9da8),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                CustomTextFormField(
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter your email address.';
+                    }
+                    if (!value.contains('@') || !value.contains('.')) {
+                      return 'Please enter a valid email address.';
+                    }
+                    return null;
+                  },
+                  controller: emailController,
+                  hintText: 'Your Email',
+                  prefix: Icons.email,
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                CustomTextFormField(
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter a password.';
+                    }
+                    return null;
+                  },
+                  controller: passwordController,
+                  hintText: 'Password',
+                  prefix: Icons.lock,
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                CustomButton(
+                  text: 'Sign In',
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const CustomDivider(titleDivider: 'OR'),
+                const SizedBox(height: 16),
+                SocialLoginButton(
+                  icon: FontAwesomeIcons.google,
+                  text: "Login with Google",
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 12),
+                SocialLoginButton(
+                  icon: FontAwesomeIcons.facebook,
+                  text: "Login with Facebook",
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                CustomTextButton(
+                  text: 'Forgot Password',
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                CustomRow(
+                  text: "Don't have an account?",
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SignUp()));
+                  },
+                  textButton: 'Register',
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/views/sign_up.dart
+++ lib/views/sign_up.dart
@@ -14,9 +14,9 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   final TextEditingController fullnameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
-
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController passwordCheckController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -25,124 +25,131 @@ class _SignUpState extends State<SignUp> {
       body: Padding(
         padding: const EdgeInsets.only(top: 120, left: 20, right: 20),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.network(
-                    'https://trangotech.com/blog/wp-content/uploads/2024/05/ecommerce-app-features.jpg'),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              const Text(
-                "Let's Get Started",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff272f54),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                      'https://trangotech.com/blog/wp-content/uploads/2024/05/ecommerce-app-features.jpg'),
                 ),
-              ),
-              const Text(
-                'Create an new account',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff9f9da8),
+                const SizedBox(
+                  height: 12,
                 ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              CustomTextFormField(
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Please enter your full name.';
-                  }
-                  return null;
-                },
-                controller: fullnameController,
-                hintText: 'Full Name',
-                prefix: Icons.person,
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              CustomTextFormField(
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Please enter your email address.';
-                  }
-                  if (!value.contains('@') || !value.contains('.')) {
-                    return 'Please enter a valid email address.';
-                  }
-                  return null;
-                },
-                controller: emailController,
-                hintText: 'Your Email',
-                prefix: Icons.email,
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              CustomTextFormField(
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Please enter a password.';
-                  }
-                  if (value.length < 8) {
-                    return 'Password must be at least 8 characters long.';
-                  }
-                  if (!RegExp(
-                          r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
-                      .hasMatch(value)) {
-                    return 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.';
-                  }
-                  if (passwordController.text != passwordCheckController.text) {
-                    return 'Passwords do not match.';
-                  }
-                  return null;
-                },
-                controller: passwordController,
-                hintText: 'Password',
-                prefix: Icons.work,
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              CustomTextFormField(
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Please confirm your password.';
-                  }
-                  if (passwordController.text != passwordCheckController.text) {
-                    return 'Passwords do not match.';
-                  }
-                  return null;
-                },
-                controller: passwordCheckController,
-                hintText: 'Password',
-                prefix: Icons.lock,
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              CustomButton(
-                text: 'Sign Up',
-                onPressed: () {},
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              CustomRow(
-                text: 'Do you have an Account?',
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const SignIn()));
-                },
-                textButton: 'Sign In',
-              )
-            ],
+                const Text(
+                  "Let's Get Started",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff272f54),
+                  ),
+                ),
+                const Text(
+                  'Create an new account',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff9f9da8),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                CustomTextFormField(
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter your full name.';
+                    }
+                    return null;
+                  },
+                  controller: fullnameController,
+                  hintText: 'Full Name',
+                  prefix: Icons.person,
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                CustomTextFormField(
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter your email address.';
+                    }
+                    if (!value.contains('@') || !value.contains('.')) {
+                      return 'Please enter a valid email address.';
+                    }
+                    return null;
+                  },
+                  controller: emailController,
+                  hintText: 'Your Email',
+                  prefix: Icons.email,
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                CustomTextFormField(
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter a password.';
+                    }
+                    if (value.length < 8) {
+                      return 'Password must be at least 8 characters long.';
+                    }
+                    if (!RegExp(
+                            r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
+                        .hasMatch(value)) {
+                      return 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.';
+                    }
+                    if (passwordController.text !=
+                        passwordCheckController.text) {
+                      return 'Passwords do not match.';
+                    }
+                    return null;
+                  },
+                  controller: passwordController,
+                  hintText: 'Password',
+                  prefix: Icons.work,
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                CustomTextFormField(
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please confirm your password.';
+                    }
+                    if (passwordController.text !=
+                        passwordCheckController.text) {
+                      return 'Passwords do not match.';
+                    }
+                    return null;
+                  },
+                  controller: passwordCheckController,
+                  hintText: 'Password',
+                  prefix: Icons.lock,
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                CustomButton(
+                  text: 'Sign Up',
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                CustomRow(
+                  text: 'Do you have an Account?',
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SignIn()));
+                  },
+                  textButton: 'Sign In',
+                )
+              ],
+            ),
           ),
         ),
       ),
